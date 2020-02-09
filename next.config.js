e4b1bb8d122b4d7a3e027a83f33ec12a5bce0db0
Andrew Nguyen <388693+andrewnguyenism@users.noreply.github.com>
2020-02-09 12:21:59 +1100

    build: prepare for deployment

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 require('dotenv').config()
 module.exports = {
   env: {
-    // Reference a variable that was defined in the .env file and make it available at Build Time
     FOURSQUARE_CLIENT_ID: process.env.FOURSQUARE_CLIENT_ID,
     FOURSQUARE_CLIENT_SECRET: process.env.FOURSQUARE_CLIENT_SECRET,
   },
